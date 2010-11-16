483e43e34ad06d9a03e6ccdd94bb952390f6c9cd
Kei Shiratsuchi <kei-s@mbc.nifty.com>
2010-11-16 20:17:17 +0900

    fetcher use operation_queue

--- processer/fetcher.rb
+++ processer/fetcher.rb
@@ -15,53 +15,78 @@ require 'ohm'
 Ohm.connect
 require_relative '../model/user'
 
-def createParser
+def stream_parser(id)
   parser = Yajl::Parser.new
   encoder = Yajl::Encoder.new
   stream = MQ.new.fanout('stream')
   parser.on_parse_complete = Proc.new {|data|
-    puts "fetched: #{Time.now} | #{data['for_user']}"
+    puts "#{id} fetched: #{Time.now} | #{data['for_user']}"
     stream.publish(encoder.encode(data).force_encoding('us-ascii'))
   }
   parser
 end
 
-def start(follows)
+def stream_request(id,follows,oauth_consumer,oauth_access_token)
+  parser = stream_parser(id)
+  request = EventMachine::HttpRequest.new("https://betastream.twitter.com/2b/site.json")
+  http = request.get(:query => {"follow" => follows.join(","), "with" => "followings"},
+                     :head => {"Content-Type" => "application/x-www-form-urlencoded"},
+                     :timeout => 90) do |client|
+    oauth_consumer.sign!(client,oauth_access_token)
+                     end
+  # Start
+  puts "#{id} Start: #{Time.now}"
+  p follows
+  http.stream do |chunk|
+    if chunk == "\n"
+      puts "#{Time.now} keep-alive"
+    else
+      parser << chunk
+    end
+  end
+  # finish
+  http.callback do
+    puts "#{id} Finished: #{Time.now}"
+  end
+  http.errback do
+    puts "#{id} Finished: #{Time.now}"
+  end
+  http
+end
+
+def follows
+  User.all.map do |user|
+    user.twitter_id
+  end
+end
+
+def start
   oauth_consumer = OAuth::Consumer.new(configatron.twitter.consumer_key,configatron.twitter.consumer_secret,:site => 'http://twitter.com')
   oauth_access_token = OAuth::AccessToken.new(oauth_consumer,configatron.twitter.access_token,configatron.twitter.access_token_secret)
 
   EM.run do
-    parser = createParser
-    request = EventMachine::HttpRequest.new("https://betastream.twitter.com/2b/site.json")
-    http = request.get(:query => {"follow" => follows.join(","), "with" => "followings"},
-                       :head => {"Content-Type" => "application/x-www-form-urlencoded"},
-                       :timeout => 90) do |client|
-      oauth_consumer.sign!(client,oauth_access_token)
-                       end
+    http = nil
+    operation_queue = MQ.queue('operation')
 
-    puts "Start: #{Time.now}"
-    http.stream do |chunk|
-      if chunk == "\n"
-        puts "#{Time.now} keep-alive"
+    id = 0
+    operation_queue.subscribe do |msg|
+      puts "Received: #{msg}"
+      case msg
+      when "start", "reload"
+        id += 1
+        http.close_connection if http
+        http = stream_request(id,follows,oauth_consumer,oauth_access_token)
+      when "stop"
+        EM.stop
       else
-        parser << chunk
+        puts "Undefined: #{msg}"
       end
     end
 
-    http.callback do
-      puts "fin"
-      EM.stop
-    end
-    http.errback do
-      puts "err"
-      EM.stop
-    end
+    operation_queue.publish("start")
   end
 end
 
 configatron.configure_from_yaml(File.dirname(__FILE__)+"/config.yml")
 raise "Add processer/config.yml" if configatron.nil?
-follows = User.all.map do |user|
-  user.twitter_id
-end
-start(follows)
+start
